d7880b04802f6f867db3175fa2e0d5cb9c44c4e7
Karen Barcelos <karenlbarcelos@hotmail.com>
2024-03-09 18:12:28 -0300

    adding status code and location to post test

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -40,7 +42,18 @@ class CashCardApplicationTests {
 	@Test
 	void shouldCreateANewCashCard() {
 		CashCard newCashCard = new CashCard(null, 250.00);
-		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
-		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards",
+				newCashCard,
+				Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		//when a POST request results in the successful creation of a resource, the response should include information
+		// for how to retrieve that resource.
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+
+		//we'll use the Location header's information to fetch the newly created CashCard.
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard,
+				String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 }
