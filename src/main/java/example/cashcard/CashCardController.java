95d86a55b61b8bad865e264b3b44aa6a7b9f79c7
Karen Barcelos <karenlbarcelos@hotmail.com>
2024-03-12 22:40:01 -0300

    implementig paging and sorting - default behavior

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -39,7 +39,8 @@ class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()));
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
         return ResponseEntity.ok(page.getContent());
     }
 
